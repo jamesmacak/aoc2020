b601e323a82485fd6ab8517b0e771de1e4eeb6bb
jamesmacak <26549968+jamesmacak@users.noreply.github.com>
2021-01-11 14:11:28 -0500

    todo: d4p2

--- Day4/day4.py
+++ Day4/day4.py
@@ -22,17 +22,44 @@ def p1():
     
     passports.append(passport) # account for the last passport in batch
     valid_passport_count = 0
+    filtered_passports = []
     for passport in passports:
         if len(passport) == 8 or (len(passport) == 7 and passport.get('cid') == None):
             valid_passport_count += 1
+            filtered_passports.append(passport)
     print(valid_passport_count)
-    return(passports)
+    return(filtered_passports)
 
 
 def p2():
-    passports = p1()
-    for passport in passports:
+    filtered_passports = p1()
+    # print(len(filtered_passports))
+    final_passports = []
+    for passport in filtered_passports:
         print(passport)
+        valid_param_count = 0
+        for k in passport: # continue to ignore cid
+            if k == 'byr' and (int(passport[k]) >= 1920 and int(passport[k]) <= 2002):
+                print('byr valid')
+                valid_param_count += 1
+            if k == 'iry' and (int(passport[k]) >= 2010 and int(passport[k]) <= 2020):
+                print('iyr valid')
+                valid_param_count += 1
+            if k == 'eyr':
+                valid_param_count += 1
+            if k == 'hgt':
+                valid_param_count += 1
+            if k == 'hcl':
+                valid_param_count += 1
+            if k == 'ecl':
+                valid_param_count += 1
+            if k == 'pid':
+                valid_param_count += 1
+        print(valid_param_count)
+        if valid_param_count >= 7: # some param bad
+            final_passports.append(passport) # add good passport
+    print(len(final_passports))
+
     
     byr_regex = re.compile(r'') # no regex, int() then check
     iyr_regex = re.compile(r'')
